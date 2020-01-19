d268d102d4b49f6bbc09ff942681fefe4990120c
tbaldeviso <tbbaldeviso@ucdavis.edu>
2020-01-19 06:41:51 -0800

    More html changes and edits to how categories are picked

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# HackDavis2020
+# Recycle Buddy - Tyler Baldeviso and Marty Macalalad
+
+Even with the newfound abundance of categroized waste bins, most people still don't discard them in the proper place.  Recycle Buddy is here to help solve this problem by to categorize images of different kinds of waste to their respective categories: Cans and Bottles, Landfill, and Compost.  This project was aimed to promote education into better recycling practicing to help contribute to growing the sustainability of our Earth.
+
+## Tools
+- Google Cloud Vision API for categorizing images
+- Javascript for front-end and back-end
+- HTML and CSS for website layout
+
+Our current implementation looks into the label descriptions in the JSON provided by Google Cloud Vision.  We compare regular expressions of common objects within each category and choose the best one accordingly.  We can improve upon this in the future by perhaps using the AutoML Google Cloud service instead to train the analyzer to categorize the waste better.
--- server.js
+++ server.js
@@ -20,6 +20,7 @@ var form = '<!DOCTYPE HTML><html><body>' +
   "<form method='post' action='/upload' enctype='multipart/form-data'>" +
   "<div><input type='file' name='image'/></div>" +
   "<input type='submit' /></form>" +
+  `<br><span>Upload an image of waste to be categorized!</span>` +
   '</body></html>';
 
 app.get('/', function(req, res) {
@@ -45,9 +46,12 @@ app.post('/upload', upload.single('image'), function(req, res, next) {
           // Base64 the image so we can display it on the page
           res.write('<img width=200 class="center" src="' + base64Image(req.file.path) + '"><br>');
 
+          // List of regex per category
           const recycleList = [/bottle/i, /can(\s|$)/i];
           const compostList = [/food/i, /soil/i, /plant/i, /leaves/i, /grass/i];
           const landfillList = [/foil/i, /wrapper/i, /plastic/i, /lid/i];
+  
+          // Filter out any labels not pertaining to any of the categories
           var recycle = labels.filter(label => recycleList.some(function(x){
             return x.test(label.description)
           }));
@@ -58,19 +62,35 @@ app.post('/upload', upload.single('image'), function(req, res, next) {
             return x.test(label.description)
           }));
 
-          if (recycle.length > 0) {
+          // Sum up scores for each category
+          var recyclescore = 0;
+          recycle.forEach(element => {
+            recyclescore = recyclescore + element.score
+          });
+          var compostscore = 0;
+          compost.forEach(element => {
+            compostscore = compostscore + element.score
+          });
+          var landfillscore = 0;
+          landfill.forEach(element => {
+            landfillscore = landfillscore + element.score
+          });
+
+          // Waste category has the highest score
+          if (recyclescore > compostscore && recyclescore > landfillscore) {
             res.write(`<span>Cans and Bottles</span>`);
-          } else if (compost.length > 0) {
+          } else if (compostscore > recyclescore && compostscore > landfillscore) {
             res.write(`<span>Compost</span>`);
-          } else if (landfill.length > 0) {
+          } else if (landfillscore > compostscore && landfillscore > recyclescore) {
             res.write(`<span>Landfill</span>`);
-          } else {
+          } else if (recyclescore == 0 && landfillscore == 0 && compostscore == 0){
             res.write(`<span>Cannot identify category</span>`);
           }
 
           // Delete file (optional)
           fs.unlinkSync(req.file.path);
       
+          // Add ending html including back button
           res.end('<br><br><a href="javascript:history.back()">Go Back</a></body></html>');
   })
   .catch(err => {
@@ -80,13 +100,14 @@ app.post('/upload', upload.single('image'), function(req, res, next) {
 
 });
 
+// Include css folder
 app.use('/css',express.static(__dirname +'/css'));
 
+// Listen to local host on port 8080
 app.listen(8080);
 console.log('Server Started in https://localhost:8080');
 
 // Turn image into Base64 so we can display it easily
-
 function base64Image(src) {
   var data = fs.readFileSync(src).toString('base64');
   return util.format('data:%s;base64,%s', mime.lookup(src), data);
